cecbbd182703f72a01c14f243e72bab67c467200
agv1992 <angel.victoria92@gmail.com>
2018-01-27 09:07:24 -0600

    Returned None if no string was found.

--- crawler.py
+++ crawler.py
@@ -5,8 +5,8 @@ def get_next_target (s):
         end_quote = s.find('"', start_link + 1)      # int representing the position in string where the next target url ends
         url = s[start_quote + 1 : end_quote]
     else:
-        url = ''
-        end_quote = -1
+        url = None
+        end_quote = 0
     return url, end_quote
 
 def rest_of_string(s, position):
@@ -14,6 +14,6 @@ def rest_of_string(s, position):
     if position < len(string):
         new_string = string[position:]
     else:
-        new_string = ''
+        new_string = None
     return new_string
     
