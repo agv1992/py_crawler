6bde72ff5b3b02fc3cca7eee0d591cca1af964fe
agv1992 <angel.victoria92@gmail.com>
2018-01-27 07:56:40 -0600

    Added logic to return empty string and -1 if next target url not found.

--- crawler.py
+++ crawler.py
@@ -1,6 +1,10 @@
 def get_next_target (s):
     start_link = s.find('<a href=')
-    start_quote = s.find('"', start_link)        # int representing the position in string where the next target url begins
-    end_quote = s.find('"', start_link + 1)      # int representing the position in string where the next target url ends
-    url = s[start_quote + 1 : end_quote]
+    if start_link > 0:
+        start_quote = s.find('"', start_link)        # int representing the position in string where the next target url begins
+        end_quote = s.find('"', start_link + 1)      # int representing the position in string where the next target url ends
+        url = s[start_quote + 1 : end_quote]
+    else:
+        url = ''
+        end_quote = -1
     return url, end_quote
